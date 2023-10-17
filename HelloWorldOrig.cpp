3b3f294a4142c024b55d7229299955da282501d4
profs <profs@DESKTOP-PC5J034>
2023-10-17 16:21:37 -0400

    Add project files.

---
+++ HelloWorldOrig.cpp
@@ -0,0 +1,13 @@
+//Leslie Spivey
+//CIS 1201
+//Oct 17, 2023
+//This program will display the message "Hello World" on the screen.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
